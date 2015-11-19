5ea17ed50e0b2718dd841cd86b68ccc2922c3027
Raquel Alvarez <rva5120@gmail.com>
2015-11-18 19:48:05 -0500

    Initial Commit

---
+++ hash_function/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hash_function
+//
+//  Created by Raquel Alvarez on 11/18/15.
+//  Copyright (c) 2015 R. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
